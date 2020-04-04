cedad41ac0ce3692849d8b5797e66ca843c0e0b0
Vitaly <vitalgolob@gmail.com>
2020-04-04 18:56:59 +0300

    add utils and colors

---
+++ src/utils/Colors.js
@@ -0,0 +1,10 @@
+export default const color = {
+  black: '#000000',
+  white: '#ffffff',
+  theme: '#232323',
+  charcoal: '$474747',
+  gray: '#7d7d7d',
+  lightshgray: '#9d9d9d',
+  smoke: '#eeeeee',
+  green: '#219653',
+};
